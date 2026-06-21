fe35b9f9b63f02110da3c2a715a6ce26d0b388ef
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-06-21 23:31:02 +0600

    feat: update home page composition with redesigned sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,18 @@
 import Banner from "@/components/home/Banner";
+import Stats from "@/components/home/Stats";
 import FeaturedProperties from "@/components/home/FeaturedProperties";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 import TopLocations from "@/components/home/TopLocations";
-import RentalStats from "@/components/home/RentalStats";
 import Reviews from "@/components/home/Reviews";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Stats />
       <FeaturedProperties />
       <WhyChooseUs />
       <TopLocations />
-      <RentalStats />
       <Reviews />
     </>
   );
