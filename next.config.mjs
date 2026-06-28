f2709d8c173342716a8b275ebacd7231e2ff0d06
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-06-28 19:07:35 +0600

    add google ibb and wildcard image domains to next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.imgbb.com",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "**.imgbb.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "**" },
     ],
   },
 };
