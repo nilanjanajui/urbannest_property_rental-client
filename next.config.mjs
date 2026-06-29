f4a3d157d2d5d2692e23af3bbafcb56675c42572
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-06-29 12:10:05 +0600

    removed Next.js image optimization entirely for external images.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "**.imgbb.com" },
